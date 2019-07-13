97e8248fe219bacfc1111e93e5185750571ff556
Messi-Q <messi.qp711@gmail.com>
2019-07-13 09:22:43 +0800

    Update hyperparamters

--- parser.py
+++ parser.py
@@ -13,6 +13,6 @@ def parameter_parser():
     parser.add_argument('-d', '--dropout', type=float, default=0.5, help='dropout rate')
     parser.add_argument('--vector_dim', type=int, default=100, help='dimensions of vector')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
-    parser.add_argument('-b', '--batch_size', type=int, default=64, help='batch size')
+    parser.add_argument('-b', '--batch_size', type=int, default=32, help='batch size')
 
     return parser.parse_args()
