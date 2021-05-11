569b5fcfd0d9079cb4526217e1ccd4e7aff10f1b
Messi-Q <969289210@qq.com>
2021-05-11 20:51:44 +0800

    Update clean_fragment.py

--- clean_fragment.py
+++ clean_fragment.py
@@ -100,19 +100,6 @@ def clean_fragment(fragment):
 
 
 if __name__ == '__main__':
-    test1 = ['contract Owner{',
-             'mapping (address => uint) private userBalances;',
-             'mapping (address => bool) private claimedBonus;',
-             'mapping (address => uint) private rewardsForA;',
-             'function WithdrawReward(address recipient) public {',
-             'uint amountToWithdraw = rewardsForA[recipient];',
-             'rewardsForA[recipient] = 0;',
-             'require(recipient.call.value(amountToWithdraw)());}',
-             ' function GetFirstWithdrawalBonus(address recipient) public {',
-             'if (claimedBonus[recipient] == false) {',
-             'throw;}',
-             'rewardsForA[recipient] += 100;',
-             'WithdrawReward(recipient);',
-             'claimedBonus[recipient] = true;}}']
+    test1 = ['if (credit[msg.sender] < amount) {']
     print(clean_fragment(test1))
 
